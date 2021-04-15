4ecc4b129bd1a52325797edd21cb77fbcc6cbd7e
alejori6 <alejori6@hotmail.com>
2021-04-14 21:47:24 -0500

    Mi primera pagina web

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+function validardatos(){
+   // alert("Hola Mundo");
+    //prompt("Ingrese su nombre");
+    //console.log("Hola Mundo");
+    //document.write("Hola Mundo");
+
+    let nombre = document.getElementById(`name`).Value
+
+    if(nombre == "cristian"){
+        alert(`Bienvenido`)
+    }else{
+        alert(`No eres Bienvenido`);
+    }
+
+
+}
